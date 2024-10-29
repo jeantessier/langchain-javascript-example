4d35e8af7f5266fcc4fb5b52191f2cfc740172ac
Jean Tessier <jean@jeantessier.com>
2024-10-29 16:45:53 -0700

    Pass in the API key explicitly

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import 'dotenv/config'
 import { ChatOpenAI } from "@langchain/openai"
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
-const model = new ChatOpenAI({ model: "gpt-4o-mini" })
+const model = new ChatOpenAI({
+    model: "gpt-4o-mini",
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
 const messages = [
     new SystemMessage("Translate the following from English into Italian"),
